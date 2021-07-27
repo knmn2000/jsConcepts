0d190703dbf2d6537caf9526edba7240e1156fd8
karan <karan@radicali.com>
2021-07-27 21:39:01 +0530

    SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5

--- README.md
+++ README.md
@@ -243,3 +243,42 @@ will be deleted, and be popped out of the call stack
 
 
 ```
+
+### SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5
+- https://www.youtube.com/watch?v=QCRpVw2KXf8&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=6&ab_channel=AkshaySaini
+
+```javascript
+//https://www.youtube.com/watch?v=QCRpVw2KXf8&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=6&ab_channel=AkshaySaini
+//SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5
+
+/*
+whenever a js program is run a global object(window), global exec context,
+and a "this" variable is created. 
+
+wherever js runs, theres a js engine running along with it. 
+chrome as v8, etc etc. 
+
+in case of browsers, this obj is called "window".
+its created even if the JS file is empty 
+
+at global level
+this===window is true 
+
+what is global space?
+any code that we write, that is not inside any function
+
+
+*/
+
+var a = 10;
+function b() {
+  var x = 10;
+}
+console.log(window.a);
+console.log(a);
+console.log(this.a);
+// ouput
+// 10
+// 10
+// 10
+```
--- script.js
+++ script.js
@@ -1,60 +1,33 @@
-// How functions work in JS ❤️ & Variable Environment | Namaste JavaScript Ep. 4
-//https://www.youtube.com/watch?v=gSDncyuGw0s&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=5&ab_channel=AkshaySaini
+//https://www.youtube.com/watch?v=QCRpVw2KXf8&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=6&ab_channel=AkshaySaini
+//SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5
 
-var x = 1;
-a();
-b();
-console.log(x);
+/*
+whenever a js program is run a global object(window), global exec context,
+and a "this" variable is created. 
 
-function a() {
-  var x = 10;
-  console.log(x);
-}
+wherever js runs, theres a js engine running along with it. 
+chrome as v8, etc etc. 
 
-function b() {
-  var x = 100;
-  console.log(x);
-}
+in case of browsers, this obj is called "window".
+its created even if the JS file is empty 
 
-// output
-// 10
-// 100
-// 1
+at global level
+this===window is true 
 
-/* 
-in the execution context-> memory will be allocated to x, a and b
-x will hold undefined 
-a and b will hold their function code
+what is global space?
+any code that we write, that is not inside any function
 
-this global execution context will be pushed to the call stack
 
-in the code, when the function a() is called, an execution context will be made for it
-and this execution context will be stored inside its parent's execution context
-
-it will also have a memory and a codesection, it will also have 
-a variable x with memory allocated to it, and it will hold "undefined" 
-in the beginning. 
-
-this new execution context will be pushed to the callstack
-
-during the execution of a()'s code, its variable x will get its value allocated to it.
-Now, the console log inside function a() will look for the variable x in the 
-local execution context.
-
-after execution of a(), the whole execution context of a() will be deleted, 
-and popped from the callstack. 
-
-now b() will be called. the same steps as above will be followed 
-create execution context within the global execution context ->
-memory will be allocated to x -> value be allocated to x -> x will be console logged 
--> x will be found in the local exec context -> this execution context will be deleted and 
-popped out of the stack.
-
-finally the console log after the function calls will be executed, 
-it will look for variable x and it will find x in the global execution context
-
-now theres nothing more to execute, so finally the global execution context 
-will be deleted, and be popped out of the call stack
 */
 
-// // // //
+var a = 10;
+function b() {
+  var x = 10;
+}
+console.log(window.a);
+console.log(a);
+console.log(this.a);
+// ouput
+// 10
+// 10
+// 10
