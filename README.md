5097d55cf39bb11a91f84527ac60714d374e5c07
karan <karan@radicali.com>
2021-08-18 21:50:56 +0530

    undefined vs not defined in JS 🤔 | Namaste JavaScript Ep. 6

--- README.md
+++ README.md
@@ -281,3 +281,34 @@ console.log(this.a);
 // 10
 // 10
 ```
+### undefined vs not defined in JS 🤔 | Namaste JavaScript Ep. 6
+- https://www.youtube.com/watch?v=QCRpVw2KXf8&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=6&ab_channel=AkshaySaini
+```javascript
+// memory will be allocated to this var even before this line is executed
+//trying to log "a" before executing this line will result in "undefined"
+
+console.log(a);
+//undefined
+var a = 7;
+console.log(a);
+//7
+
+// console.log(x);
+//script.js:10 Uncaught ReferenceError: x is not defined
+
+//undefined is a special keyword, it is assigned to vars until they are defiend
+
+var b;
+if (b === undefined) {
+  console.log('b is undefined');
+} else {
+  console.log('b is not undefined');
+}
+//b is undefined
+
+var c;
+c = undefined;
+//^ this is a total no-no. doesnt make sense to do this.
+//  use null or similar keywords to say that the variable doesnt hold anything
+
+```
--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-//https://www.youtube.com/watch?v=QCRpVw2KXf8&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=6&ab_channel=AkshaySaini
-//SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5
+//https://www.youtube.com/watch?v=B7iF6G3EyIk&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=8&ab_channel=AkshaySaini
+//undefined vs not defined in JS 🤔 | Namaste JavaScript Ep. 6
 
-/*
-whenever a js program is run a global object(window), global exec context,
-and a "this" variable is created. 
+// memory will be allocated to this var even before this line is executed
+//trying to log "a" before executing this line will result in "undefined"
 
-wherever js runs, theres a js engine running along with it. 
-chrome as v8, etc etc. 
-
-in case of browsers, this obj is called "window".
-its created even if the JS file is empty 
-
-at global level
-this===window is true 
-
-what is global space?
-any code that we write, that is not inside any function
-
-
-*/
-
-var a = 10;
-function b() {
-  var x = 10;
-}
-console.log(window.a);
 console.log(a);
-console.log(this.a);
-// ouput
-// 10
-// 10
-// 10
+//undefined
+var a = 7;
+console.log(a);
+//7
+
+// console.log(x);
+//script.js:10 Uncaught ReferenceError: x is not defined
+
+//undefined is a special keyword, it is assigned to vars until they are defiend
+
+var b;
+if (b === undefined) {
+  console.log('b is undefined');
+} else {
+  console.log('b is not undefined');
+}
+//b is undefined
+
+var c;
+c = undefined;
+//^ this is a total no-no. doesnt make sense to do this.
+//  use null or similar keywords to say that the variable doesnt hold anything
